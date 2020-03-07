aaf62e35256ff45be8acadf4ebf9423deeac6686
Balintataw J <balintataw2@gmail.com>
2020-03-07 14:07:21 -0800

    Added oktokit

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 const run = () => {
-  const token = core.getInput("repo-token");
-  console.log("TOKEN", token);
+  try {
+    const token = core.getInput("repo-token");
+    const octokit = new github.GitHub(token);
+    const { data: reviewers } = await octokit.pulls.list({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner
+    });
+    // reviwers[0].requested_reviewers reutrns array of objects of revieweres
+    // login from that object
+    // loop through that array to get everyones login
+    // reviewers.url is link to pull request
+    console.log(reviewers[0].requested_reviewers);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 };
 
 run();
