d1e5ea318d8a5c8a4f6852dbb2eddefb18bf4362
Balintataw J <balintataw2@gmail.com>
2020-03-07 13:35:35 -0800

    Added npm init

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
 
+const run = () => {
+  const token = core.getInput("repo-token");
+  console.log("TOKEN", token);
+};
+
+run();
